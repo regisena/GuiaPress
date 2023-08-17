7a7abb37995d2c0783150049e163be867e4ac1e8
REGI SENA <regsena@gmail.com>
2023-08-17 05:47:23 -0400

    primeiro versionamento no github
    primeiro versionamento no github do GuiaPress

---
+++ articles/Article.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = require("../categories/Category");
+
+const Article = connection.define("articles", {
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Category.hasMany(Article); // Tem muitos / 1(categoria) --> M(artigos)
+Article.belongsTo(Category); // Um artigo pertende a uma categoria / 1(artigo) --> 1(categoria)
+
+// Article.sync({force: true}); //comentado para não ficar recriando esta tabela
+
+module.exports = Article;
