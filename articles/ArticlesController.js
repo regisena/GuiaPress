9de6019d03a1957351dd7235328517f3190d996e
REGI SENA <regsena@gmail.com>
2023-08-20 20:18:18 -0400

    Seção 7 Aula 94

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
     res.send("ROTA DE ARTIGOS");
@@ -12,4 +14,16 @@ router.get("/admin/articles/new", (req, res) => {
     })
 });
 
+router.post("/articles/save",(req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    })
+});
 module.exports = router;
