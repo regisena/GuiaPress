8c717d92b8d027464d3b76a4bf0434050d950bdc
REGI SENA <regsena@gmail.com>
2023-08-22 21:59:09 -0400

    Seção 7 Aula 99

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,9 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-    Article.findAll().then(articles => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
         res.render("admin/articles/index", {articles: articles});
     });
 });
