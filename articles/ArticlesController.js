d3e9635102f6083051912ead006a1827ae4422e0
REGI SENA <regsena@gmail.com>
2023-08-23 21:50:49 -0400

    Seção 7 Aula 100

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -33,4 +33,23 @@ router.post("/articles/save",(req, res) => {
     });
 });
 
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){ 
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() =>{
+                res.redirect("/admin/articles");
+            });
+        }else{ // não for um número
+            res.redirect("/admin/articles");
+        }
+    }else{ // null
+        res.redirect("/admin/articles");
+    }
+});
+
 module.exports = router;
