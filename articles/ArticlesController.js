0b0ec6969552f43b8592a92bbc3ce94dda5e7865
REGI SENA <regsena@gmail.com>
2023-09-07 06:29:09 -0400

    Seção 7 Aula 115

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -110,6 +110,7 @@ router.get("/articles/page/:num", (req, res) => {
         }
 
         var result = {
+            page: parseInt(page),
             next: next,
             articles: articles,
         }
